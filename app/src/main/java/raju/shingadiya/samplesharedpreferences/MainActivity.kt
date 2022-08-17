317ea399918a6cbe7fe5192eca58d33e6d90c46f
Raju Shingadiya <rajushingadiya.main@gmail.com>
2022-08-17 18:55:20 +0530

    updated readme for publish 1.0.1 changes

--- app/src/main/java/raju/shingadiya/samplesharedpreferences/MainActivity.kt
+++ app/src/main/java/raju/shingadiya/samplesharedpreferences/MainActivity.kt
@@ -18,7 +18,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         initView()
- 
+
     }
 
     private fun initView() {
